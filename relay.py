937a803683275d020a9139fd1ccfab8c34644665
cpzt <chenpan9012@gmail.com>
2023-12-22 15:01:54 +0800

    update relay

--- relay.py
+++ relay.py
@@ -1,16 +1,6 @@
 import machine
 import time
 
-# 设置继电器所连接的GPIO引脚
-relay_pin = machine.Pin(17, machine.Pin.OUT)  # 根据实际连接选择引脚
-
-# 控制继电器打开
-relay_pin.value(1)
-time.sleep(2)  # 等待2秒
-
-# 控制继电器关闭
-relay_pin.value(0)
-
 
 class Relay:
     def __init__(self, gpio):
@@ -22,7 +12,7 @@ class Relay:
     def off(self):
         return self.pin.off()
 
-    def open_once(self, ts=2000):
+    def open_once(self, ts=1000):
         """
         :param ts: milliseconds since the epoch
         :return:
